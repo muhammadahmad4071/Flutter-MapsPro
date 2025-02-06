20e0774d519b61be8c2593c9f5cce84afcc18f14
user <user@192.168.10.29>
2025-02-06 15:32:34 +0500

    ios real device testing in progress

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps  // Add this for Google Maps
+import FirebaseCore // Add this for Firebase
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +9,13 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    // Initialize Google Maps with your API Key
+    GMSServices.provideAPIKey("AIzaSyBsVw09Zl_Xby65X7ed8Xs2ov8aAhaWiFk")
+    
+    // Initialize Firebase
+    FirebaseApp.configure()
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
